a0ea28c9151f65c291342fe067437436ee1c8b73
MarcioMoura2022 <86208649+MarcioMoura2022@users.noreply.github.com>
2023-04-13 11:51:51 -0300

    .

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 #chmod +x 
 
 
-git clone https://github.com/AHDSF/bombcrypto-superbotmaster.git
+git clone https://github.com/MarcioMoura2022/bombcrypto-superbot.git
 cd bombcrypto-superbotmaster
 yarn install
 yarn start:init
